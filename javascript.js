13b5a03b26e8f80256a41c5a5751d5f70003cd48
Atakan <atakanyaln@gmail.com>
2022-12-02 10:37:03 +0300

    added some flavor with random colors still needs work

--- javascript.js
+++ javascript.js
@@ -2,7 +2,8 @@
 const boxes = document.getElementsByClassName("boxes")
 const r = document.querySelector(':root');
 
-createBox(100)
+//default index//
+createBoxDefault(100)
 
 //event listener for button//
 document.getElementById("btn").addEventListener("click", createBox)
@@ -10,11 +11,11 @@ document.getElementById("btn").addEventListener("click", createBox)
 //box maker//
 function createBox() {
     //reset boxes//
-    boxes[0].replaceChildren(userInput);
+    boxes[0].replaceChildren();
     //get prompt//
     let userInput = prompt("How many boxes do you want on each side?");
     //calculate height and width values//
-    let heightWidth = 1600/userInput
+    let heightWidth = 1600 / userInput
     //change css variable for grid//
     r.style.setProperty('--cssGrid', `repeat(${userInput},1fr)`);
     //calculate the area of grid//
@@ -31,8 +32,40 @@ function createBox() {
     //event listener for paint//
     let box = document.getElementsByClassName("box")
     for (let i = 0; i < box.length; i++) {
-        box[i].addEventListener("mouseover", function(){
-            box[i].style.backgroundColor = "red";
+        box[i].addEventListener("mouseover", function () {
+            let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            box[i].style.backgroundColor = `#${randomColor}`;
         })
     }
 }
+
+//seperate function for defualt index//
+function createBoxDefault(userInput) {
+    //reset boxes//
+    boxes[0].replaceChildren();
+    //calculate height and width values//
+    let heightWidth = 1600 / userInput
+    //change css variable for grid//
+    r.style.setProperty('--cssGrid', `repeat(${userInput},1fr)`);
+    //calculate the area of grid//
+    gridArea = userInput * userInput
+    //create box divs//
+    for (i = 0; i < gridArea; i++) {
+        let box = document.createElement("div")
+        box.setAttribute("class", "box")
+        //set box height and width//
+        box.style.minHeight = `${heightWidth}px`
+        box.style.minWidth = `${heightWidth}px`
+        boxes[0].appendChild(box)
+    }
+    //event listener for paint//
+    let box = document.getElementsByClassName("box")
+    for (let i = 0; i < box.length; i++) {
+        box[i].addEventListener("mouseover", function () {
+            let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            box[i].style.backgroundColor = `#${randomColor}`;
+        })
+}
+}
+
+
